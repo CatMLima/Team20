ab4f914c79480c2581c77b7514784fc07fc74035
brynjar <brynjar23@live.com>
2024-10-09 21:42:03 +0000

    Created feed.html page and added controller methods to StudyActivityController

--- src/main/java/is/hi/hbv501g/team20/Controllers/StudyActivityController.java
+++ src/main/java/is/hi/hbv501g/team20/Controllers/StudyActivityController.java
@@ -1,7 +1,9 @@
 package is.hi.hbv501g.team20.Controllers;
 
 import is.hi.hbv501g.team20.Persistence.Entities.StudyActivity;
+import is.hi.hbv501g.team20.Persistence.Entities.User;
 import is.hi.hbv501g.team20.Services.StudyActivityService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,4 +34,29 @@ public class StudyActivityController {
         studyActivityService.save(studyActivity);
         return "redirect:/home";
     }
+    // Feed page stuff is here below
+    // Displays feed page
+    @RequestMapping("/feed")
+    public String showFeed(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
+
+        return "feed";
+    }
+
+    // Displays user profile page
+    @RequestMapping("/profile")
+    public String showProfile() {
+        return "user";
+    }
+
+    // Create a study activity
+    @RequestMapping("/create-study")
+    public String startStudyActivity() {
+        return "studyactivity";
+    }
+    //End of feed page stuff
 }
